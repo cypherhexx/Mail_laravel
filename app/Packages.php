000fca2e0efb3cdaf7139f91381dbe12b7803ae1
shilpavijyn <shilpa@bpract.com>
2020-02-17 10:45:11 +0530

    a

--- app/Packages.php
+++ app/Packages.php
@@ -59,6 +59,7 @@ class Packages extends Model
               $package=ProfileInfo::where('user_id',$upuser)->value('package');
               $pack=Packages::find($package);
               $level_commission=$package_am*$pack->level_percent*0.01;
+              if($commision > 0){
                 $commision = Commission::create([
                 'user_id'        => $upuser,
                 'from_id'        => $user_id,
@@ -73,6 +74,7 @@ class Packages extends Model
           * updates the userbalance
           */
           User::upadteUserBalance($upuser, $level_commission);
+        }
           self::checkRankbasedCommission($upuser,$package_am,$user_id);
           }
 
@@ -83,6 +85,7 @@ class Packages extends Model
         if($rank > 1){
            $rankgain=Ranksetting::find($rank)->gain;
            $rank_commission=$amount*$rankgain*0.01;
+           if($commision > 0){
                     $commision = Commission::create([
                     'user_id'        => $user,
                     'from_id'        => $from,
@@ -94,6 +97,7 @@ class Packages extends Model
                     'payment_status' => 'Yes',
               ]);
               User::upadteUserBalance($user, $rank_commission);
+            }
 
           }
      }
