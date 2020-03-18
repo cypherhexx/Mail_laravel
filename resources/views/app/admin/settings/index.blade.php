a9fdba2733a73f500f141750101696650002ad30
anittabpractian <anitta@bpract.com>
2020-03-18 18:35:32 +0530

    settings

--- resources/views/app/admin/settings/index.blade.php
+++ resources/views/app/admin/settings/index.blade.php
@@ -144,161 +144,7 @@
                                         </div>
                                     </div>  
 
-
-                                  
-                                    <!--   <div class="form-group">
-                                         <div class="row">
-                                            <div class="col-sm-6">
-                                                <label for="">Three Friends (%):</label>
-                                            </div>
-                                            <div class="col-sm-4">
-                                                 <a class="settings form-control"  id="three_friends" data-type='text' data-pk="{{$settings->id}}" data-title='three friends' data-name="three_friends">
-                                                 {{ $settings->three_friends}}
-                                                </a>
-
-
-                                            </div>
-                                           
-                                        </div>
-                                    </div>    -->
-<!-- 
-                                         <div class="form-group">
-                                         <div class="row">
-                                            <div class="col-sm-6">
-                                                <label for="">Eight Friends (%):</label>
-                                            </div>
-                                            <div class="col-sm-4">
-                                                 <a class="settings form-control"  id="eight_friends" data-type='text' data-pk="{{$settings->id}}" data-title='eight friends' data-name="eight_friends">
-                                                 {{ $settings->eight_friends}}
-                                                </a>
-
-
-                                            </div>
-                                           
-                                        </div>
-                                    </div>   -->
-
-                                 
-                              
-
-                              
-          
-
-
-                      <div class="panel panel-flat" >
-                        <div class="panel-body"> 
-                          <form id="settings">                             
-                        <legend>{{trans('C-Extra bonus')}}</legend>
-                                       
-                                <fieldset>   
-
-
-                                    <div class="form-group">
-                                         <div class="row">
-                                            <div class="col-sm-6">
-                                                <label for="">Trader (%):</label>
-                                            </div>
-                                            <div class="col-sm-4">
-                                                 <a class="settings form-control"  id="trader" data-type='text' data-pk="{{$settings->id}}" data-title='trader' data-name="trader">
-                                                 {{ $settings->trader}}
-                                                </a>
-
-
-                                            </div>
-                                           
-                                        </div>
-                                    </div>   
-
-
-
-
-                                   
-
-                                      <div class="form-group">
-                                         <div class="row">
-                                            <div class="col-sm-6">
-                                                <label for="">Star (%):</label>
-                                            </div>
-                                            <div class="col-sm-4">
-                                                 <a class="settings form-control"  id="star" data-type='text' data-pk="{{$settings->id}}" data-title='star' data-name="star">
-                                                 {{ $settings->star}}
-                                                </a>
-
-
-                                            </div>
-                                           
-                                        </div>
-                                    </div>   
-
-                                      <div class="form-group">
-                                         <div class="row">
-                                            <div class="col-sm-6">
-                                                <label for="">Superstar (%):</label>
-                                            </div>
-                                            <div class="col-sm-4">
-                                                 <a class="settings form-control"  id="superstar" data-type='text' data-pk="{{$settings->id}}" data-title='superstar' data-name="superstar">
-                                                 {{ $settings->superstar}}
-                                                </a>
-
-
-                                            </div>
-
-
-                                            <br><br> <br><br> 
-
-                                            <table class="table table-striped">
-                            <thead> 
-                                <th>{{ trans('Matrix') }} </th>
-                                <th>{{ trans('Percent (%)') }}</th>
-                                
-                           <!--      <th>{{ trans('packages.revenue_share_rs') }}</th>
-                                <th>{{ trans('packages.binary_percentage') }} </th>                                
-                                <th>{{ trans('packages.daily_pv_limit') }} </th> -->                                
-                            </thead>
-                            <tbody>
-                                @foreach($sett as $item)
-
-                                <tr>
-                                    <td>  <a class="settings" id="settings{{$item->id}}" data-type='text' data-pk="{{$item->id}}" data-title="Enter  matrix level " data-name="package">
-                                                
-                                              {{$item->matrixlevel}}  </a> </td>
-
-                                    
-
-                                    <td><a class="settings" id="level_percent{{$item->id}}" data-type='text' data-pk="{{$item->id}}" data-title="Enter level percent" data-name="level_percent">
-                                                
-                                           {{$item->cpercent}} </a> </td>
-
-                                
-                                           
-                                </tr> 
-
-
-                                @endforeach
-                                
-                            </tbody>
-
-
-                          </table>                           
-                                           
-                                        </div>
-                                    </div>        
-
-                                </fieldset>                            
-                 
-                     
-                    </form>
-                        
-                        
-                       
-                </div>
-              
-
-            </div>
-
-
-
-             <div class="panel panel-flat" >
+                                      <div class="panel panel-flat" >
                         
 
 
@@ -346,7 +192,223 @@
                         </div>
 
 
+
 </div><br><br>
+
+
+                                  
+                                    <!--   <div class="form-group">
+                                         <div class="row">
+                                            <div class="col-sm-6">
+                                                <label for="">Three Friends (%):</label>
+                                            </div>
+                                            <div class="col-sm-4">
+                                                 <a class="settings form-control"  id="three_friends" data-type='text' data-pk="{{$settings->id}}" data-title='three friends' data-name="three_friends">
+                                                 {{ $settings->three_friends}}
+                                                </a>
+
+
+                                            </div>
+                                           
+                                        </div>
+                                    </div>    -->
+<!-- 
+                                         <div class="form-group">
+                                         <div class="row">
+                                            <div class="col-sm-6">
+                                                <label for="">Eight Friends (%):</label>
+                                            </div>
+                                            <div class="col-sm-4">
+                                                 <a class="settings form-control"  id="eight_friends" data-type='text' data-pk="{{$settings->id}}" data-title='eight friends' data-name="eight_friends">
+                                                 {{ $settings->eight_friends}}
+                                                </a>
+
+
+                                            </div>
+                                           
+                                        </div>
+                                    </div>   -->
+
+                                 
+                              
+
+                              
+<!--           <div class="panel panel-flat" >
+    <div class="panel-heading">
+      <h4 class="panel-title">
+         <div class="heading-elements"> 
+        <button id="enable_settings1" type="submit" class="btn btn-primary">{{trans('packages.enable_edit_mode')}}</button>
+       
+      </div>
+    </div>
+    <div class="panel-body"> 
+        <form id="settings" method="post">  
+        <table class="table table-striped">
+          <thead> 
+             <th>{{ trans('Category') }} </th>
+             <th>{{ trans('Percentage') }} </th>
+            
+          </thead>
+          <tbody>                
+        @foreach($category as $item)
+            <tr>
+                <td>{{$item->category_name}}</td>   
+                <td>  <a class="settings" id="settings{{$item->id}}" data-type='text' data-pk="{{$item->id}}" data-title="percentage" data-name="percentage">
+
+                {{$item->percentage}}  </a> 
+               </td>
+                </tr> 
+          @endforeach
+          </tbody>
+        </table>
+      </form>
+    </div>
+  </div>
+ -->
+
+                      <div class="panel panel-flat" >
+                        <div class="panel-body"> 
+                          <form id="settings">                             
+                        
+                                 
+                            <!--  <form id="settings">
+                                             <legend>{{trans('C-Extra bonus')}}</legend>
+
+                                            <table class="table table-striped">
+                            <thead> 
+                                <th>{{ trans('Category') }} </th>
+                                <th>{{ trans('Percentage') }} </th>
+                                                              
+                            </thead>
+                            <tbody>
+                                @foreach($category as $item)
+
+                                <tr>
+                                    <td>  
+                                  
+                                              {{$item->category_name}} 
+                                                </td>    
+                                    <td>  <a class="settings" id="settings{{$item->id}}" data-type='text' data-pk="{{$item->id}}" data-title="percentage" data-name="percentage">
+                                                
+                                              {{$item->percentage}}  </a> </td>
+
+
+
+                                   @endforeach
+                               </tr>
+                           </tbody>
+                       </table>
+                   </form>
+      -->
+
+                                <!-- <fieldset>    -->
+
+
+                                    
+
+
+                                   
+
+                                     
+
+                                    
+
+
+                                            <br><br> <br><br> 
+
+                                            <table class="table table-striped">
+                            <thead> 
+                                <th>{{ trans('Matrix') }} </th>
+                                <th>{{ trans('Percent (%)') }}</th>
+                                
+                                                          
+                            </thead>
+                            <tbody>
+                                @foreach($sett as $item)
+
+                                <tr>
+                                    <td>  <a class="settings" id="settings{{$item->id}}" data-type='text' data-pk="{{$item->id}}" data-title="Enter  matrix level " data-name="package">
+                                                
+                                              {{$item->matrixlevel}}  </a> </td>
+
+                                    
+
+                                    <td><a class="settings" id="level_percent{{$item->id}}" data-type='text' data-pk="{{$item->id}}" data-title="Enter level percent" data-name="level_percent">
+                                                
+                                           {{$item->cpercent}} </a> </td>
+
+                                
+                                           
+                                </tr> 
+
+
+                                @endforeach
+                                
+                            </tbody>
+
+
+                          </table>                           
+                                           
+                                        </div>
+                                    </div>        
+
+                                </fieldset>                            
+                 
+                     
+                    </form>
+
+                   <div class="panel panel-flat" >
+                    <div class="panel-heading">
+                      <h4 class="panel-title">
+                         <div class="heading-elements"> 
+                        <button id="enable_settings1" type="submit" class="btn btn-primary">{{trans('packages.enable_edit_mode')}}</button>
+                       
+                      </div>
+                    </div>
+                    <div class="panel-body"> 
+                                                 
+                        
+                                 
+                             <form id="settings1">
+                                             <legend>{{trans('C-Extra bonus')}}</legend>
+
+                                            <table class="table table-striped">
+                            <thead> 
+                                <th>{{ trans('Category') }} </th>
+                                <th>{{ trans('Percentage') }} </th>
+                                                              
+                            </thead>
+                            <tbody>
+                                @foreach($category as $item)
+
+                                <tr>
+                                    <td>  
+                                  
+                                              {{$item->category_name}} 
+                                                </td>    
+                                    <td>  <a class="settings1" id="settings1{{$item->id}}" data-type='text' data-pk="{{$item->id}}" data-title="percentage" data-name="percentage">
+                                                
+                                              {{$item->percentage}}  </a> </td>
+
+
+
+                                   @endforeach
+                               </tr>
+                           </tbody>
+                       </table>
+                   </form>
+                        
+                        
+                       
+                </div>
+              
+
+            </div>
+
+
+
+
+           
 
 
 
