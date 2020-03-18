7cf4a01e484c7e1eacb1b9212e3e0c515e4900cd
anittabpractian <anitta@bpract.com>
2020-03-18 19:05:11 +0530

    cat

---
+++ database/migrations/2020_03_18_070838_category.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Category extends Migration
+{
+    /**
+     * Run txhe migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('category_name');
+            $table->double('count');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('category');
+    }
+}
