0171cf16cee4aa409feb0171a6c1d7bd0abd3c19
root <root@server.algolight.net>
2020-03-27 10:10:53 +0000

    set

--- resources/views/app/admin/settings/index.blade.php
+++ resources/views/app/admin/settings/index.blade.php
@@ -68,7 +68,7 @@
                                       <div class="form-group">
                                          <div class="row">
                                             <div class="col-sm-6">
-                                                <label for="">Joining Fee Referral (%):</label>
+                                                <label for="">Joining Fee:</label>
                                             </div>
                                             <div class="col-sm-4">
                                                  <a class="settings form-control"  id="joinfee" data-type='text' data-pk="{{$settings->id}}" data-title='Joining Fee' data-name="joinfee">
