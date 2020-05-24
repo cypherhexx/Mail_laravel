7f233817e608f9626adbca3a2cad729888ef01bf
BluePrince123 <turanskiserhii0228@gmail.com>
2020-05-24 13:35:20 +0000

    fixing the link

---
+++ storage/framework/views/1fc75126eac4c04fc254987f193a2bcb82747749.php
@@ -0,0 +1,58 @@
+
+
+<?php $__env->startSection('content'); ?>
+<!-- Page container -->
+<div class="page-container">
+    <!-- Page content -->
+    <div class="page-content">
+        <!-- Main content -->
+        <div class="content-wrapper">
+            <!-- Content area -->
+            <div class="content">
+                <!-- Error title -->
+                <div class="text-center content-group">
+                    <h1 class="error-title">
+                        404
+                    </h1>
+                    <h5>
+                        Oops, That page is not found, maybe for now!
+                    </h5>
+                </div>
+                <!-- /error title -->
+                <!-- Error content -->
+                <div class="row">
+                    <div class="col-lg-4 col-lg-offset-4 col-sm-6 col-sm-offset-3">
+                        <div class="row">
+                            <div class="col-sm-6 col-sm-offset-3">
+                                <a class="btn btn-primary btn-block content-group" href="<?php echo e(url('/')); ?>">
+                                    <i class="icon-circle-left2 position-left">
+                                    </i>
+                                    <?php echo e(trans('all.go_home')); ?>
+
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- /error wrapper -->
+            </div>
+            <!-- /content area -->
+        </div>
+        <!-- /main content -->
+    </div>
+    <!-- /page content -->
+</div>
+<!-- /page container -->
+<?php $__env->stopSection(); ?>
+<?php $__env->startSection('styles'); ?>
+##parent-placeholder-bf62280f159b1468fff0c96540f3989d41279669##
+<style type="text/css">
+</style>
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('scripts'); ?>
+<script type="text/javascript">
+</script>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('no-layout', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
--- storage/framework/views/855f13ba18675ee583633b6133aa649e286236cf.php
+++ storage/framework/views/855f13ba18675ee583633b6133aa649e286236cf.php
@@ -1,2 +1,100 @@
- <?php $__env->startSection('title'); ?> <?php echo e($title); ?> :: ##parent-placeholder-3c6de1b7dd91465d437ef415f94f36afc1fbc8a8## <?php $__env->stopSection(); ?> <?php $__env->startSection('styles'); ?> <link href="<?php echo e(asset('assets/globals/plugins/x-editables/css/bootstrap-editable.css')); ?>" rel="stylesheet"/> <link href="<?php echo e(asset('assets/admin/css/plugins/bootstrap-wysihtml5-0.0.3.css')); ?>" rel="stylesheet"/> <?php $__env->stopSection(); ?> <?php $__env->startSection('main'); ?> <div class="panel panel-flat" > <div class="panel-heading"> <h4 class="panel-title"><?php echo e(trans('ticket_config.welcome_email')); ?></h4> <div class="heading-elements"> <button id="enable" class="btn btn-primary"><?php echo e(trans('settings.enable_edit_mode')); ?></button> </div> </div> <div class="panel-body"> <form id="settings"> <legend><?php echo e(trans('ticket_config.welcome_email')); ?></legend> <?php $__currentLoopData = $settings; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $rank): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?> <div class="col-sm-12"> <div class="form-group"> <div class="row"> <div class="col-sm-4"> <label class="form-label" for="point_value"><?php echo e(trans('ticket_config.subject')); ?>:</label> </div> <div class="col-sm-8"> <a class="settings form-control" data-pk="<?php echo e($rank->id); ?>" data-url='<?php echo e(url("admin/welcomeemail")); ?>' data-type='text' id="subject" data-title='Enter subject' data-name="subject" style="min-height:58px;min-width:50px"><?php echo $rank->subject; ?> </a> </div> </div> </div> <div class="form-group"> <div class="row"> <div class="col-sm-4"> <label class="form-label" for="point_value"><?php echo e(trans('ticket_config.welcome_email_message')); ?>:</label> </div> <div class="col-sm-8" > <div class="settings form-control" data-url='<?php echo e(url("admin/welcomeemail")); ?>' data-pk="<?php echo e($rank->id); ?>" data-type='wysihtml5' id="note" data-title='Enter content' data-name="body" style="min-height:200px;min-width:50px"><?php echo $rank->body; ?></div> </div> </div> </div> </div> </form> </div> <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?> </div> <div class="panel panel-flat" > <div class="panel-heading"> <h4 class="panel-title">Payout Notification Settings</h4> <div class="heading-elements"> <button id="enable-payout-settings-edit" class="btn btn-primary"><?php echo e(trans('settings.enable_edit_mode')); ?></button> </div> </div> <div class="panel-body"> <form id="settings_pay"> <legend>Payout Notification Settings</legend> <?php $__currentLoopData = $settings_payout; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $rank): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?> <div class="col-sm-12"> <div class="form-group"> <div class="row"> <div class="col-sm-4"> <label class="form-label" for="point_value"><?php echo e(trans('ticket_config.subject')); ?>:</label> </div> <div class="col-sm-8"> <a class="settings_pay form-control" data-pk="<?php echo e($rank->id); ?>" data-url='<?php echo e(url("admin/payoutnotification")); ?>' data-type='text' id="subject" data-title='Enter subject' data-name="subject"><?php echo e($rank->subject); ?> </a> </div> </div> </div> <div class="form-group"> <div class="row"> <div class="col-sm-4"> <label class="form-label" for="point_value">Mail Content:</label> </div> <div class="col-sm-8" > <div class="settings_pay form-control" data-url='<?php echo e(url("admin/payoutnotification")); ?>' data-pk="<?php echo e($rank->id); ?>" data-type='wysihtml5' id="note" data-title='Enter content' data-name="mail_content" style="min-height:200px;min-width:50px"><?php echo $rank->mail_content; ?></div> </div> </div> </div> </div> </form> <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?> </div> </div> <?php $__env->stopSection(); ?> <?php $__env->startSection('scripts'); ?> ##parent-placeholder-16728d18790deb58b3b8c1df74f06e536b532695## <script src="<?php echo e(asset('assets/admin/js/welcome-settings-editable.js')); ?>"></script> <?php $__env->stopSection(); ?> 
+<?php $__env->startSection('title'); ?> <?php echo e($title); ?> :: ##parent-placeholder-3c6de1b7dd91465d437ef415f94f36afc1fbc8a8## <?php $__env->stopSection(); ?>
+
+
+<?php $__env->startSection('styles'); ?>
+
+<link href="<?php echo e(asset('assets/globals/plugins/x-editables/css/bootstrap-editable.css')); ?>" rel="stylesheet"/>
+
+<link href="<?php echo e(asset('assets/admin/css/plugins/bootstrap-wysihtml5-0.0.3.css')); ?>" rel="stylesheet"/>
+
+
+<?php $__env->stopSection(); ?>
+
+
+<?php $__env->startSection('main'); ?>
+
+<h1>Welcome to the Edit template page.</h1>
+<form action="<?php echo e(url('admin/welcomeemail')); ?>" method="post" data-parsley-validate="true" name="form-wizard">
+ <input type="hidden" name="_token" value="<?php echo e(csrf_token()); ?>"> 
+  <p></p>
+  <div style="margin-bottom: 5px">
+  <span style="font-size: 20px; font-weight: bold; vertical-align: middle; ">Subject: </span><input type="text" name="subject" value="">
+  </div>
+  <br>
+  <textarea id="bodyField" name="content"></textarea>
+  <input type="submit" value="submit">
+</form>
+
+
+
+<table class="table datatable-basic table-striped table-hover" id="templates-table">
+    <thead>
+        <tr>
+            <th>
+                No
+            </th>
+            <th>
+                Mail_title
+            </th>
+            <th>
+                Content
+            </th>
+            <th>
+                Edit
+            </th>
+        </tr>
+    </thead>
+    <tbody>
+    <?php $__currentLoopData = $templates; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $key=> $template): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+        <tr>
+          <th>
+            <?php echo e($key + 1); ?>
+
+          </th>
+          <th>
+            <?php echo e($template->subject); ?>
+
+          </th>
+          <th>
+            <?php echo $template->text; ?>
+
+          </th>
+          <th>
+             <a  class="btn btn-sm btn-primary m-b-10" href="<?php echo e(URL::to('admin/edittemplate/'.$template->id)); ?>"><i class="icon-pencil4"></i></a>
+            <a class="btn btn-sm btn-primary m-b-10" href="<?php echo e(URL::to('admin/deletetemplate/'.$template->id)); ?>"><i class="fa fa-trash"></i></a>
+          </th>
+        </tr>
+      <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+    </tbody>
+</table>
+
+
+ <script type="text/javascript">
+$(document).on('submit', 'form', function() {
+   $(this).find('button:submit, input:submit').attr('disabled','disabled');
+ });
+</script>
+
+<?=app(JeroenNoten\LaravelCkEditor\CkEditor::class)->editor('bodyField');?>;
+
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('scripts'); ?> ##parent-placeholder-16728d18790deb58b3b8c1df74f06e536b532695##
+      <script src="<?php echo e(asset('assets/admin/js/welcome-settings-editable.js')); ?>"></script>
+    <?php $__env->stopSection(); ?> 
+<!-- <script type="text/javascript">
+      window.onload = function () {
+          
+              var edt = CKEDITOR.replace('bodyField', { toolbar: 'Basic' });
+CKFinder.setupCKEditor(edt, '/ckfinder/');
+
+              var t = <%="how to set the data" %>;
+              CKEDITOR.instances.editor1.setData(t);
+              
+}
+<script type="text/javascript">
+
+  CKEDITOR.instances['bodyField'].setData("how to start the email template");
+
+</script> -->
 <?php echo $__env->make('app.admin.layouts.default', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
