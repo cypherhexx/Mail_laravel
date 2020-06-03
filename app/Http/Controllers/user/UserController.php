aa7d013163db59e8634fe50cdf5dd2dea7066bd7
BluePrince123 <turanskiserhii0228@gmail.com>
2020-06-03 03:13:34 -0700

    change the style of error message in download

--- app/Http/Controllers/user/UserController.php
+++ app/Http/Controllers/user/UserController.php
@@ -205,11 +205,11 @@ class UserController extends UserAdminController
           error_log("testing loginsg");
           return  Response::download($file,"NOXVM.exe", $headers);
         }else {
-          Session::flash('flash_notification',array('message'=>"You do not purchase the package",'level'=>'warning'));
+          Session::flash('flash_notification',array('message'=>"Please purchase the package.",'level'=>'error'));
              return  redirect("user/runsoftware");
         }
       }else {
-        Session::flash('flash_notification',array('message'=>"You do not purchase the package",'level'=>'warning'));
+        Session::flash('flash_notification',array('message'=>"Please purchase the package",'level'=>'error'));
              return  redirect("user/runsoftware");
       }
     }
